fb540698b200c9feaa8db5327681e331d80f566a
Dalmo Silva Mendes <65993036+DalmoMendes@users.noreply.github.com>
2023-10-16 22:52:14 -0300

    Add files via upload

---
+++ 03-C/ProgrmaQuebraLinha.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    printf("Sistema de Notas \n Versão 1.0 \n Desenvolvido por: Dsmsoft \n Boa Vista-RR, 2023");
+
+    return 0;
+}
