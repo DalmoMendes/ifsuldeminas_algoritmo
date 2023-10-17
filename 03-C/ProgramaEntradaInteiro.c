6d003ea8b33ab39018641470a1d66ae38534baeb
Dalmo Silva Mendes <65993036+DalmoMendes@users.noreply.github.com>
2023-10-16 22:26:56 -0300

    Add files via upload

---
+++ 03-C/ProgramaEntradaInteiro.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+  
+    printf("Digite um valor: ");
+    scanf("%d",&numero);
+    
+    printf("O valor do úmero digitado é: %d", numero);
+
+    return 0;
+}
