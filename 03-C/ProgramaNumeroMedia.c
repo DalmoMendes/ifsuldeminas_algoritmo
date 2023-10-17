d5df3fe35f0cca515e9100636999b5798e0b451f
Dalmo Silva Mendes <65993036+DalmoMendes@users.noreply.github.com>
2023-10-16 21:43:46 -0300

    Add files via upload

---
+++ 03-C/ProgramaNumeroMedia.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    float media;
+    
+    media = 7.5;
+    //numero = 2;
+    
+    //printf("Este é o número: %d", numero);
+    printf("Essa é a média da sua nota: %f", media);
+
+    return 0;
+}
