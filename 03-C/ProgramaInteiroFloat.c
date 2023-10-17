7a26f4c829b574c7ef2b6b35b1184e236b8189f2
Dalmo Silva Mendes <65993036+DalmoMendes@users.noreply.github.com>
2023-10-16 23:03:19 -0300

    Add files via upload

---
+++ 03-C/ProgramaInteiroFloat.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    int x;
+    float z;
+
+    printf("Digite um valor inteiro:");
+    scanf("%d", &x);
+    printf("Digite um valor real:");
+    scanf("%f", &z);
+    printf("X = %d – Y = %f", x, z);
+    
+    printf("\n\nSistema de Notas \n Versão 1.0 \n Desenvolvido por: Dsmsoft \n Boa Vista-RR, 2023");
+
+    return 0;
+}
